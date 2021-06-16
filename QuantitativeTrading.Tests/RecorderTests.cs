2d883c4ae02d1ca6e97f4d4dd5c39ff79a504579
KennethTang <kenneth@nssh.onmicrosoft.com>
2021-06-16 23:28:16 +0800

    Fix test failed.

--- QuantitativeTrading.Tests/RecorderTests.cs
+++ QuantitativeTrading.Tests/RecorderTests.cs
@@ -17,18 +17,18 @@ namespace QuantitativeTrading.Tests
         {
             Recorder<ThreeMarketsModel> recorder = new("Test", AppDomain.CurrentDomain.BaseDirectory);
             DateTime date = DateTime.Now;
-            recorder.Insert(new ThreeMarketsModel{ Close = 1, Balance = 2, Assets = 3, Date = date });
-            recorder.Insert(new ThreeMarketsModel{ Close = 4, Balance = 5, Assets = 6, Date = date });
+            recorder.Insert(new ThreeMarketsModel{ CoinBalance1 = 1, Balance = 2, Assets = 3, Date = date });
+            recorder.Insert(new ThreeMarketsModel{ CoinBalance1 = 4, Balance = 5, Assets = 6, Date = date });
             await recorder.SaveAsync();
 
             IImporter importer = new CsvImporter();
             var result = (await importer.Import<ThreeMarketsModel>(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Test.csv"))).Data.ToList();
             Assert.AreEqual(date.ToString(), result[0].Date.ToString());
-            Assert.AreEqual(1, result[0].Close);
+            Assert.AreEqual(1, result[0].CoinBalance1);
             Assert.AreEqual(2, result[0].Balance);
             Assert.AreEqual(3, result[0].Assets);
             Assert.AreEqual(date.ToString(), result[1].Date.ToString());
-            Assert.AreEqual(4, result[1].Close);
+            Assert.AreEqual(4, result[1].CoinBalance1);
             Assert.AreEqual(5, result[1].Balance);
             Assert.AreEqual(6, result[1].Assets);
         }
