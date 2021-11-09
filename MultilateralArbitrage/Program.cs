dc575c3aabffd2c44d3bc2a3fd6a7868b1ed7ed3
Kenneth Tang <kenneth@nssh.onmicrosoft.com>
2021-11-10 00:52:25 +0800

    Get all symbols from binance spot

---
+++ MultilateralArbitrage/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Binance.Net;
+using Binance.Net.Objects.Spot.MarketData;
+using CryptoExchange.Net.Objects;
+
+namespace MultilateralArbitrage;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        BinanceClient client = new();
+        WebCallResult<BinanceExchangeInfo> webCallResult = await client.Spot.System.GetExchangeInfoAsync();
+        if (!webCallResult.Success)
+        {
+            Console.WriteLine(webCallResult.Error!.Message);
+            return;
+        }
+
+        BinanceExchangeInfo binanceExchangeInfo = webCallResult.Data;
+
+        Dictionary<string, List<BinanceSymbol>> symbols = new();
+        foreach (BinanceSymbol symbol in binanceExchangeInfo.Symbols)
+        {
+            if (!symbols.ContainsKey(symbol.BaseAsset))
+                symbols.Add(symbol.BaseAsset, new());
+            if (!symbols.ContainsKey(symbol.QuoteAsset))
+                symbols.Add(symbol.QuoteAsset, new());
+            symbols[symbol.BaseAsset].Add(symbol);
+            symbols[symbol.QuoteAsset].Add(symbol);
+        }
+
+
+        Console.WriteLine(string.Join('\n', binanceExchangeInfo.Symbols.Select(item => item.Name)));
+        Console.WriteLine(binanceExchangeInfo.Symbols.Count());
+        Console.ReadKey();
+    }
+}
