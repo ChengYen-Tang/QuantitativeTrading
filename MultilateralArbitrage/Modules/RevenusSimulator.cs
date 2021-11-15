f19c8817cf23d3af51b5d01a184eaa54cf3a4aba
Kenneth Tang <kenneth@nssh.onmicrosoft.com>
2021-11-16 02:09:41 +0800

    Revenus simulator

---
+++ MultilateralArbitrage/Modules/RevenusSimulator.cs
@@ -0,0 +1,45 @@
+﻿using MultilateralArbitrage.Models;
+
+namespace MultilateralArbitrage.Modules
+{
+    internal class RevenusSimulator
+    {
+        private readonly decimal fee;
+        public ICollection<ICollection<Symbol>> AllMarketMix { get; set; }
+        public RevenusSimulator(ICollection<ICollection<Symbol>> allMarketMix, int fee)
+            => (AllMarketMix, this.fee) = (allMarketMix, fee / 100);
+
+        public async Task<ICollection<(ICollection<Symbol> marketMix, decimal assets)>> CalculateAllIncomeAsync(string startAsset, IDictionary<string, OrderBook> orderBooks)
+        {
+            IEnumerable<Task<(ICollection<Symbol> marketMix, decimal asset)>> tasks = AllMarketMix.AsParallel().Select(item => Task.Run<(ICollection<Symbol> marketMix, decimal asset)>(() => (item, CalculateIncome(startAsset, item, orderBooks))));
+            return await Task.WhenAll(tasks);
+        }
+
+        public decimal CalculateIncome(string startAsset, ICollection<Symbol> marketMix, IDictionary<string, OrderBook> orderBooks)
+        {
+            decimal initialAssets = 1000000;
+            decimal assets = initialAssets;
+            string coin = startAsset;
+            foreach (Symbol symbol in marketMix)
+            {
+                if (coin != symbol.BaseAsset && coin != symbol.QuoteAsset)
+                    throw new ArgumentException($"Coin: {coin} not in symbol: {symbol.Name}, MarketMix: {string.Join(", ", marketMix.Select(item => item.Name))}");
+                if (orderBooks[symbol.Name].AskPrice is 0 || orderBooks[symbol.Name].BidPrice is 0)
+                    return decimal.MinValue;
+                if (coin == symbol.QuoteAsset)
+                {
+                    assets /= orderBooks[symbol.Name].AskPrice;
+                    coin = symbol.BaseAsset;
+                }
+                else if (coin == symbol.BaseAsset)
+                {
+                    assets *= orderBooks[symbol.Name].BidPrice;
+                    coin = symbol.QuoteAsset;
+                }
+                assets -= assets * fee;
+            }
+
+            return (assets - initialAssets) / initialAssets * 100;
+        }
+    }
+}
--- MultilateralArbitrage/Program.cs
+++ MultilateralArbitrage/Program.cs
@@ -12,11 +12,15 @@ public class Program
         ICollection<Symbol> symbols = await api.DownloadSymbolsAsync();
         IDictionary<string, OrderBook> orderBooks = await api.GetAllOrderBooksAsync();
         IDictionary<string, ICollection<Symbol>> classificationSymbols = symbols.ToClassificationSymbols();
-        MarketMix marketMix = new(classificationSymbols, 5);
+        MarketMix marketMix = new(classificationSymbols, 4);
         ICollection<ICollection<Symbol>> allMarketMix = marketMix.GetAllMarketMix("BUSD");
         Console.WriteLine($"市場數量: {symbols.Count}");
         Console.WriteLine($"訂單書的市場數量: {orderBooks.Keys.Count}");
         Console.WriteLine($"組合數量: {allMarketMix.Count}");
+        RevenusSimulator simulator = new(allMarketMix, 1);
+        IEnumerable<(ICollection<Symbol> marketMix, decimal assets)> revenus = (await simulator.CalculateAllIncomeAsync("BUSD", orderBooks)).OrderByDescending(item => item.assets).Take(10);
+        foreach ((ICollection<Symbol> marketMix, decimal assets) result in revenus)
+            Console.WriteLine($"Asset: {result.assets}%, MarketMix: {string.Join(", ", result.marketMix.Select(item => item.Name))}");
         Console.ReadKey();
     }
 }
