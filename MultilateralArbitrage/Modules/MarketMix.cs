951a78754053d850eec8bc53e702bb5abcf668ec
Kenneth Tang <kenneth@nssh.onmicrosoft.com>
2021-12-13 22:06:20 +0800

    Test collision and comment

--- MultilateralArbitrage/Modules/MarketMix.cs
+++ MultilateralArbitrage/Modules/MarketMix.cs
@@ -5,18 +5,37 @@ using System.Linq;
 
 namespace MultilateralArbitrage.Modules
 {
+    /// <summary>
+    /// 產生投資組合的模組
+    /// </summary>
     internal class MarketMix
     {
         private readonly ICollection<string> excludeCoin;
         private readonly IDictionary<string, ICollection<Symbol>> symbols;
         private readonly int depth;
 
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        /// <param name="symbols"> 所有市場 </param>
+        /// <param name="depth"> 瓊舉深度 </param>
         public MarketMix(IDictionary<string, ICollection<Symbol>> symbols, int depth = 0)
             : this(symbols, Array.Empty<string>(), depth) {}
 
+        /// <summary>
+        /// 初始化
+        /// </summary>
+        /// <param name="symbols"> 所有市場 </param>
+        /// <param name="excludeCoin"> 想要排除的市場 </param>
+        /// <param name="depth"> 瓊舉深度 </param>
         public MarketMix(IDictionary<string, ICollection<Symbol>> symbols, ICollection<string> excludeCoin, int depth = 0)
             => (this.symbols, this.excludeCoin, this.depth) = (symbols, excludeCoin, depth);
 
+        /// <summary>
+        /// 產生投資組合
+        /// </summary>
+        /// <param name="startAsset"> 開始與結束的貨幣 </param>
+        /// <returns></returns>
         public ICollection<ICollection<Symbol>> GetAllMarketMix(string startAsset)
         {
             List<ICollection<Symbol>> allMarketMix = new();
