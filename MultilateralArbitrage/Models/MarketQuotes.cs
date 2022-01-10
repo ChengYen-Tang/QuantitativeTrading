7006eff9c56a796c06516b2a3aad95f36ac2437d
Kenneth Tang <kenneth@nssh.onmicrosoft.com>
2022-01-11 01:39:10 +0800

    交易 api

--- MultilateralArbitrage/Models/MarketQuotes.cs
+++ MultilateralArbitrage/Models/MarketQuotes.cs
@@ -2,6 +2,7 @@
 
 namespace MultilateralArbitrage.Models
 {
+    internal record Order(Symbol Symbol, bool IsBuy, bool IsImmediatelyMatch, decimal Price);
     internal record OrderBook(decimal BidPrice, decimal BidQuantity, decimal AskPrice, decimal AskQuantity, DateTime? Timestamp);
     internal record LatestPrice(decimal Price, DateTime? Timestamp);
 }
--- MultilateralArbitrage/Modules/API/Binance.cs
+++ MultilateralArbitrage/Modules/API/Binance.cs
@@ -1,21 +1,52 @@
 ﻿using Binance.Net;
+using Binance.Net.Enums;
 using Binance.Net.Objects.Spot.MarketData;
+using Binance.Net.Objects.Spot.SpotData;
+using CryptoExchange.Net.Authentication;
 using CryptoExchange.Net.Objects;
+using CryptoExchange.Net.Sockets;
+using EasyCaching.Core;
 using MultilateralArbitrage.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MultilateralArbitrage.Modules.API
 {
     internal class Binance : IAPI, IDisposable
     {
+        private readonly IEasyCachingProvider easyCaching;
+        private readonly ApiCredentials credentials;
         private readonly BinanceClient client;
+        private readonly BinanceSocketClient socketClient;
+        private readonly UpdateSubscription updateSubscription;
         private bool disposedValue;
 
         public Binance()
             => (client) = (new());
+
+        public Binance(string key, string secret, IEasyCachingProviderFactory easyCaching)
+        {
+            this.easyCaching = easyCaching.GetCachingProvider("default1");
+            credentials = new(key, secret);
+            client = new(new() { ApiCredentials = credentials });
+            socketClient = new(new() { ApiCredentials = credentials });
+            WebCallResult<string> keyResult = client.Spot.UserStream.StartUserStreamAsync().Result;
+            if (keyResult.Success)
+            {
+                CallResult<UpdateSubscription>  updateSubscriptionResult = socketClient.Spot.SubscribeToUserDataUpdatesAsync(keyResult.Data, (order) =>
+                {
+                    while (!this.easyCaching.TrySet(order.Data.OrderId.ToString(), order.Data.IsWorking, TimeSpan.FromDays(1))) ;
+                }, null, null, null).Result;
+                if (updateSubscriptionResult.Success)
+                {
+                    updateSubscription = updateSubscriptionResult.Data;
+                }
+            }
+                
+        }
 
         public async Task<ICollection<Symbol>> DownloadSymbolsAsync()
         {
@@ -47,6 +78,43 @@ namespace MultilateralArbitrage.Modules.API
             return prices.ToDictionary(item => item.Symbol, item => new LatestPrice(item.Price, item.Timestamp));
         }
 
+        public async Task MovingBricks(ICollection<Order> orders)
+        {
+            foreach (Order order in orders)
+            {
+                BinancePlacedOrder placedOrder = await PlaceOrder(order);
+                SpinWait.SpinUntil(() => { var result = easyCaching.Get<bool>(placedOrder.OrderId.ToString()); return !result.Value; });
+            }
+        }
+
+        private async Task<BinancePlacedOrder> PlaceOrder(Order order)
+        {
+            WebCallResult<BinancePlacedOrder> result;
+
+            if (order.IsBuy)
+            {
+                decimal balance = await GetBalances(order.Symbol.QuoteAsset);
+                result = order.IsImmediatelyMatch
+                    ? await client.Spot.Order.PlaceOrderAsync(order.Symbol.Name, OrderSide.Buy, OrderType.Market, quoteOrderQuantity: balance)
+                    : await client.Spot.Order.PlaceOrderAsync(order.Symbol.Name, OrderSide.Buy, OrderType.LimitMaker, quoteOrderQuantity: balance, price: order.Price);
+            }
+            else
+            {
+                decimal balance = await GetBalances(order.Symbol.BaseAsset);
+                result = order.IsImmediatelyMatch
+                    ? await client.Spot.Order.PlaceOrderAsync(order.Symbol.Name, OrderSide.Sell, OrderType.Market, balance)
+                    : await client.Spot.Order.PlaceOrderAsync(order.Symbol.Name, OrderSide.Sell, OrderType.LimitMaker, balance, price: order.Price);
+            }
+
+            return result.Data;
+        }
+
+        private async Task<decimal> GetBalances(string asset)
+        {
+            WebCallResult<BinanceAccountInfo> result = await client.General.GetAccountInfoAsync();
+            return result.Data.Balances.First(item => item.Asset == asset).Free;
+        }
+
         protected virtual void Dispose(bool disposing)
         {
             if (!disposedValue)
@@ -54,7 +122,10 @@ namespace MultilateralArbitrage.Modules.API
                 if (disposing)
                 {
                     // TODO: 處置受控狀態 (受控物件)
-                    client.Dispose();
+                    updateSubscription.CloseAsync().Wait();
+                    client?.Dispose();
+                    socketClient?.Dispose();
+                    credentials?.Dispose();
                 }
 
                 // TODO: 釋出非受控資源 (非受控物件) 並覆寫完成項
